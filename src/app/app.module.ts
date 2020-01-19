5b7e1941f44a52b29176e7926a583957513667f8
kravitzy <Kravitzy@users.noreply.github.com>
2020-01-19 21:19:24 +0200

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,38 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { environment } from '../environments/environment';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import {MaterialModule } from './modules/material/material.module';
+
+import { AngularFireModule } from 'angularfire2';
+
+import { AngularFirestoreModule  } from 'angularfire2/firestore';
+import { LayoutComponent } from './components/layout/layout.component';
+import { MainComponent } from './components/main/main.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { Page404Component } from './components/page404/page404.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LayoutComponent,
+    MainComponent,
+    Page404Component,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MaterialModule,
+    // AngularFireModule.initializeApp(environment.firebaseConfig),
+    AngularFirestoreModule,
+    LayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
